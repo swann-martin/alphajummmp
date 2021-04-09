5e7ac248650c9e87b74f426ccb56f96a435e7821
swann-martin <swannmartin1988@gmail.com>
2021-04-09 16:37:49 +0200

    initial commit

---
+++ src/Form/XpType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Xp;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class XpType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('jobDateFrom')
+            ->add('jobDateTo')
+            ->add('jobName')
+            ->add('jobLocation')
+            ->add('jobDescription')
+            ->add('userId')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Xp::class,
+        ]);
+    }
+}
